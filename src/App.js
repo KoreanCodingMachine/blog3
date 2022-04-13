0a7dce9880fa4c996670e07b3a70038dc5517670
KoreanCodingMachine <59787529+KoreanCodingMachine@users.noreply.github.com>
2022-04-13 19:10:03 +0900

    react-practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [title, setTitle] = useState([
+    '강남우동맛집',
+    '아니 뭐해 우리팀',
+    '뭐하냐고',
+  ]);
+  let [modal, setModal] = useState(false);
+  let [clicked_num, setClicked_num] = useState(0);
+  let [clicked_title, setClicked_title] = useState(0);
+  let [inputVal, setInputVal] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='black-nav'>
+        <div>개발 blog</div>
+      </div>
+      {title.map((a, i) => {
+        return (
+          <div className='list' key={i}>
+            <h3
+              onClick={() => {
+                setClicked_title(i);
+              }}
+            >
+              {a}
+              <span
+                onClick={() => {
+                  setClicked_num(clicked_num + 1);
+                }}
+              >
+                👍
+              </span>
+              {clicked_num}
+            </h3>
+            <p>2월 17일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+      {modal === true ? (
+        <Modal title={title} clicked_title={clicked_title} />
+      ) : null}
+      <button
+        type='button'
+        onClick={() => {
+          setModal(!modal);
+        }}
+      >
+        모달창
+      </button>
+      <div className='publish'>
+        <input
+          onChange={(e) => {
+            setInputVal(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            let new_title = [...title];
+            new_title.push(inputVal);
+            setTitle(new_title);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          저장
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className='modal'>
+      <h2>{props.title[props.clicked_title]}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
